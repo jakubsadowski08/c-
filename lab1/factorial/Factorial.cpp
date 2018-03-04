bb79f201ea25f30cbae9ca0b5feee9bab980ab9f
jakubsadowski <jakubsadowski08@gmail.com>
2018-03-05 00:32:23 +0100

    factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -2,13 +2,17 @@
 // Created by mwypych on 02.02.17.
 //
 #include "Factorial.h"
+#include<cmath>
 
 int factorial(int value) {
-    if(value == 0)
-        return 1;
-    else if(value < 0)
-        return value * factorial(value + 1);
-    else
-        return value * factorial(value - 1);
-    return 0;
+    if(fabs(value) >= 13)
+        return 0;
+    else {
+        if (value == 0)
+            return 1;
+        else if (value < 0)
+            return value * factorial(value + 1);
+        else
+            return value * factorial(value - 1);
+    }
 }
